180905ecba43d2e8b29240fd00052b4a4054e073
JimChenWYU <imjimchen@163.com>
2024-09-24 15:09:31 +0800

    POSIX Shell

--- scripts/probe.sh
+++ scripts/probe.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+
+# Please use POSIX Shell
+# https://www.grymoire.com/Unix/Sh.html
 
 IP=127.0.0.1
 PORT=9000
 
 while true; do
     nc -zv ${IP} ${PORT}
-    if [[ "$?" == "0" ]]; then
+    if [ "$?" == "0" ]; then
         exit 0
     fi
     sleep 2
