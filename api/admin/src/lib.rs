16d66a420646a91b07e4d75259b348be7806fdd6
bestgopher <84328409@qq.com>
2024-10-29 20:48:33 +0800

    fix: blocking forever
    Closes #102
    Signed-off-by: bestgopher <84328409@qq.com>

--- api/admin/src/lib.rs
+++ api/admin/src/lib.rs
@@ -1,26 +1,21 @@
 pub mod error;
 pub mod handlers;
-pub mod object_api;
 
 use axum::{extract::Request, response::Response, routing::get, BoxError, Router};
 use ecstore::store::ECStore;
 use error::ErrorCode;
 use handlers::list_pools;
-use object_api::ObjectApi;
 use tower::Service;
 
 pub type Result<T> = std::result::Result<T, ErrorCode>;
 
 const API_VERSION: &str = "/v3";
 
-pub fn register_admin_router(
-    ec_store: Option<ECStore>,
-) -> impl Service<Request, Response = Response, Error: Into<BoxError>, Future: Send> + Clone {
+pub fn register_admin_router() -> impl Service<Request, Response = Response, Error: Into<BoxError>, Future: Send> + Clone {
     Router::new()
         .nest(
             "/rustfs/admin",
             Router::new().nest(API_VERSION, Router::new().route("/pools/list", get(list_pools::handler))),
         )
-        .with_state::<()>(ObjectApi::new(ec_store))
         .into_service()
 }
