9f9be9d1a20a3e133f8a5df46d3c3149869f2e28
weisd <weishidavip@163.com>
2024-11-28 09:43:37 +0800

    fix get_net_info

--- madmin/src/net/mod.rs
+++ madmin/src/net/mod.rs
@@ -3,7 +3,25 @@ use serde::{Deserialize, Serialize};
 use crate::health::NodeCommon;
 
 #[cfg(target_os = "linux")]
-pub mod net_linux;
+pub fn get_net_info(addr: &str, iface: &str) -> NetInfo {
+    let mut ni = NetInfo::default();
+    ni.node_common.addr = addr.to_string();
+    ni.interface = iface.to_string();
+
+    ni
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn get_net_info(addr: &str, iface: &str) -> NetInfo {
+    NetInfo {
+        node_common: NodeCommon {
+            addr: addr.to_owned(),
+            error: "Not implemented for non-linux platforms".to_owned(),
+        },
+        interface: iface.to_owned(),
+        ..Default::default()
+    }
+}
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct NetInfo {
