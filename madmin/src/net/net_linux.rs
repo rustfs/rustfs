9e6ec15f32ee36ae47b916c02de541fc8796646e
weisd <weishidavip@163.com>
2024-11-28 09:44:59 +0800

    fix get_net_info

--- madmin/src/net/net_linux.rs
+++
@@ -1,9 +0,0 @@
-use super::NetInfo;
-
-pub fn get_net_info(addr: &str, iface: &str) -> NetInfo {
-    let mut ni = NetInfo::default();
-    ni.node_common.addr = addr.to_string();
-    ni.interface = iface.to_string();
-
-    ni
-}
