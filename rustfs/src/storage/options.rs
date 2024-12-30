f0fe3e30af93cdfbfec2316e52a8e58d3a834bba
weisd <im@weisd.in>
2024-12-30 13:59:14 +0800

    feat:#165 copyobject v1

--- rustfs/src/storage/options.rs
+++ rustfs/src/storage/options.rs
@@ -56,6 +56,55 @@ pub async fn del_opts(
     Ok(opts)
 }
 
+pub async fn get_opts(
+    bucket: &str,
+    object: &str,
+    vid: Option<String>,
+    part_num: Option<usize>,
+    headers: &HeaderMap<HeaderValue>,
+) -> Result<ObjectOptions> {
+    let versioned = BucketVersioningSys::prefix_enabled(bucket, object).await;
+    let version_suspended = BucketVersioningSys::prefix_suspended(bucket, object).await;
+
+    let vid = vid.map(|v| v.as_str().trim().to_owned());
+
+    if let Some(ref id) = vid {
+        if let Err(_err) = Uuid::parse_str(id.as_str()) {
+            return Err(Error::new(StorageError::InvalidVersionID(
+                bucket.to_owned(),
+                object.to_owned(),
+                id.clone(),
+            )));
+        }
+
+        if !versioned {
+            return Err(Error::new(StorageError::InvalidArgument(
+                bucket.to_owned(),
+                object.to_owned(),
+                id.clone(),
+            )));
+        }
+    }
+
+    let mut opts = get_default_opts(headers, None, false)
+        .map_err(|err| Error::new(StorageError::InvalidArgument(bucket.to_owned(), object.to_owned(), err.to_string())))?;
+
+    opts.version_id = {
+        if is_dir_object(object) && vid.is_none() {
+            Some(Uuid::nil().to_string())
+        } else {
+            vid
+        }
+    };
+
+    opts.part_number = part_num;
+
+    opts.version_suspended = version_suspended;
+    opts.versioned = versioned;
+
+    Ok(opts)
+}
+
 pub async fn put_opts(
     bucket: &str,
     object: &str,
@@ -102,18 +151,30 @@ pub async fn put_opts(
     Ok(opts)
 }
 
+pub async fn copy_dst_opts(
+    bucket: &str,
+    object: &str,
+    vid: Option<String>,
+    headers: &HeaderMap<HeaderValue>,
+    metadata: Option<HashMap<String, String>>,
+) -> Result<ObjectOptions> {
+    put_opts(bucket, object, vid, headers, metadata).await
+}
+
+pub fn copy_src_opts(_bucket: &str, _object: &str, headers: &HeaderMap<HeaderValue>) -> Result<ObjectOptions> {
+    get_default_opts(headers, None, false)
+}
+
 pub fn put_opts_from_headers(
     headers: &HeaderMap<HeaderValue>,
     metadata: Option<HashMap<String, String>>,
 ) -> Result<ObjectOptions> {
-    let metadata = metadata.unwrap_or_default();
-
     get_default_opts(headers, metadata, false)
 }
 
-fn get_default_opts(
+pub fn get_default_opts(
     _headers: &HeaderMap<HeaderValue>,
-    metadata: HashMap<String, String>,
+    metadata: Option<HashMap<String, String>>,
     _copy_source: bool,
 ) -> Result<ObjectOptions> {
     Ok(ObjectOptions {
