4a0bc24aa6fb75b8de5ee3fd67ce2990895ed0d7
weisd <im@weisd.in>
2025-02-24 14:25:56 +0800

    fix:#226 complete_multipart_upload output etag

--- rustfs/src/storage/ecfs.rs
+++ rustfs/src/storage/ecfs.rs
@@ -952,7 +952,7 @@ impl S3 for FS {
             return Err(S3Error::with_message(S3ErrorCode::InternalError, "Not init".to_string()));
         };
 
-        store
+        let oi = store
             .complete_multipart_upload(&bucket, &key, &upload_id, uploaded_parts, opts)
             .await
             .map_err(to_s3_error)?;
@@ -960,6 +960,7 @@ impl S3 for FS {
         let output = CompleteMultipartUploadOutput {
             bucket: Some(bucket),
             key: Some(key),
+            e_tag: oi.etag,
             ..Default::default()
         };
         Ok(S3Response::new(output))
