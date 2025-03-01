6dbf7d8d6564dea5fbd0cf6fa6b2816272ef4745
Nugine <nugine@foxmail.com>
2025-03-02 02:14:41 +0800

    ci: reduce setup time

--- scripts/e2e-run.sh
+++ scripts/e2e-run.sh
@@ -3,9 +3,20 @@ BIN=$1
 VOLUME=$2
 
 chmod +x $BIN
-sudo mkdir -p $VOLUME
+mkdir -p $VOLUME
 
 export RUST_LOG="rustfs=debug,ecstore=debug,s3s=debug,iam=debug"
 export RUST_BACKTRACE=full
+$BIN $VOLUME > /tmp/rustfs.log 2>&1 &
 
-sudo nohup $BIN $VOLUME > /tmp/rustfs.log 2>&1 &
+sleep 10
+
+export AWS_ACCESS_KEY_ID=rustfsadmin
+export AWS_SECRET_ACCESS_KEY=rustfsadmin
+export AWS_REGION=us-east-1
+export AWS_ENDPOINT_URL=http://localhost:9000
+export RUST_LOG="s3s_e2e=debug,s3s_test=info,s3s=debug"
+export RUST_BACKTRACE=full
+s3s-e2e
+
+killall $BIN
