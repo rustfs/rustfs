3b2df514a720e4283f21363a32c1b94f2cd0793d
houseme <housemecn@gmail.com>
2025-03-12 00:46:01 +0800

    improve code for observability

--- packages/obs/src/lib.rs
+++ packages/obs/src/lib.rs
@@ -1,190 +1,32 @@
-//! Logging utilities
-
+/// # obs
 ///
-/// This crate provides utilities for logging.
-///
-/// # Examples
-/// ```
-/// use rustfs_obs::{log_info, log_error};
-///
-/// log_info("This is an informational message");
-/// log_error("This is an error message");
-/// ```
-#[cfg(feature = "audit-kafka")]
-pub use audit::KafkaAuditTarget;
-#[cfg(feature = "audit-webhook")]
-pub use audit::WebhookAuditTarget;
-pub use audit::{AuditEntry, AuditLogger, AuditTarget, FileAuditTarget};
-pub use logger::{log_debug, log_error, log_info};
-pub use telemetry::Telemetry;
-
-mod audit;
+/// `obs` is a logging and observability library for Rust.
+/// It provides a simple and easy-to-use interface for logging and observability.
+/// It is built on top of the `log` crate and `opentelemetry` crate.
+mod config;
+mod entry;
 mod logger;
+mod sink;
 mod telemetry;
+mod utils;
+mod worker;
 
-#[cfg(test)]
-mod tests {
-    use crate::{log_info, AuditEntry, AuditLogger, AuditTarget, FileAuditTarget, Telemetry};
-    use chrono::Utc;
-    use opentelemetry::global;
-    use opentelemetry::trace::{TraceContextExt, Tracer};
-    use std::time::{Duration, SystemTime};
-    use tracing::{instrument, Span};
-    use tracing_opentelemetry::OpenTelemetrySpanExt;
+pub use config::load_config;
+pub use config::{AppConfig, OtelConfig};
+pub use entry::{LogEntry, SerializableLevel};
+pub use logger::start_logger;
+pub use logger::{LogError, Logger};
+pub use sink::Sink;
+pub use telemetry::init_telemetry;
+pub use utils::{get_local_ip, get_local_ip_with_default};
+pub use worker::start_worker;
 
-    #[instrument(fields(bucket, object, user))]
-    async fn put_object(audit_logger: &AuditLogger, bucket: String, object: String, user: String) {
-        let start_time = SystemTime::now();
-        log_info("Starting PUT operation");
-
-        // Simulate the operation
-        tokio::time::sleep(Duration::from_millis(100)).await;
-
-        // Record Metrics
-        let meter = global::meter("rustfs.rs");
-        let request_duration = meter.f64_histogram("s3_request_duration_seconds").build();
-        request_duration.record(
-            start_time.elapsed().unwrap().as_secs_f64(),
-            &[opentelemetry::KeyValue::new("operation", "put_object")],
-        );
-
-        // Gets the current span
-        let span = Span::current();
-
-        // Use 'OpenTelemetrySpanExt' to get 'SpanContext'
-        let span_context = span.context(); // Get context via OpenTelemetrySpanExt
-        let span_id = span_context.span().span_context().span_id().to_string(); // Get the SpanId
-
-        // Audit events are logged
-        let audit_entry = AuditEntry {
-            version: "1.0".to_string(),
-            event_type: "s3_put_object".to_string(),
-            bucket,
-            object,
-            user,
-            time: Utc::now().to_rfc3339(),
-            user_agent: "rustfs.rs-client".to_string(),
-            span_id,
-        };
-        audit_logger.log(audit_entry).await;
-
-        log_info("PUT operation completed");
-    }
-
-    #[tokio::test]
-    // #[cfg(feature = "audit-webhook")]
-    // #[cfg(feature = "audit-kafka")]
-    async fn test_main() {
-        let telemetry = Telemetry::init();
-
-        // Initialize multiple audit objectives
-        let audit_targets: Vec<Box<dyn AuditTarget>> = vec![
-            Box::new(FileAuditTarget),
-            // Box::new(KafkaAuditTarget::new("localhost:9092", "minio-audit")),
-            // Box::new(WebhookAuditTarget::new("http://localhost:8080/audit")),
-        ];
-        let audit_logger = AuditLogger::new(audit_targets);
-
-        // 创建根 Span 并执行操作
-        // let tracer = global::tracer("main");
-        // tracer.in_span("main_operation", |cx| {
-        //     Span::current().set_parent(cx);
-        //     log_info("Starting test async");
-        //     tokio::runtime::Runtime::new().unwrap().block_on(async {
-        log_info("Starting test");
-        // Test the PUT operation
-        put_object(&audit_logger, "my-bucket".to_string(), "my-object.txt".to_string(), "user123".to_string()).await;
-        tokio::time::sleep(Duration::from_millis(100)).await;
-        query_object(&audit_logger, "my-bucket".to_string(), "my-object.txt".to_string(), "user123".to_string()).await;
-        tokio::time::sleep(Duration::from_millis(100)).await;
-        for i in 0..100 {
-            put_object(
-                &audit_logger,
-                format!("my-bucket-{}", i),
-                format!("my-object-{}", i),
-                "user123".to_string(),
-            )
-            .await;
-            tokio::time::sleep(Duration::from_millis(100)).await;
-            query_object(
-                &audit_logger,
-                format!("my-bucket-{}", i),
-                format!("my-object-{}", i),
-                "user123".to_string(),
-            )
-            .await;
-            tokio::time::sleep(Duration::from_millis(100)).await;
-        }
-
-        // Wait for the export to complete
-        tokio::time::sleep(Duration::from_secs(2)).await;
-        log_info("Test completed");
-        //     });
-        // });
-        drop(telemetry); // Make sure to clean up
-    }
-
-    #[instrument(fields(bucket, object, user))]
-    async fn query_object(audit_logger: &AuditLogger, bucket: String, object: String, user: String) {
-        let start_time = SystemTime::now();
-        log_info("Starting query operation");
-
-        // Simulate the operation
-        tokio::time::sleep(Duration::from_millis(100)).await;
-
-        // Record Metrics
-        let meter = global::meter("rustfs.rs");
-        let request_duration = meter.f64_histogram("s3_request_duration_seconds").build();
-        request_duration.record(
-            start_time.elapsed().unwrap().as_secs_f64(),
-            &[opentelemetry::KeyValue::new("operation", "query_object")],
-        );
-
-        // Gets the current span
-        let span = Span::current();
-        // Use 'OpenTelemetrySpanExt' to get 'SpanContext'
-        let span_context = span.context(); // Get context via OpenTelemetrySpanExt
-        let span_id = span_context.span().span_context().span_id().to_string(); // Get the SpanId
-        query_one(user.clone());
-        query_two(user.clone());
-        query_three(user.clone());
-        // Audit events are logged
-        let audit_entry = AuditEntry {
-            version: "1.0".to_string(),
-            event_type: "s3_query_object".to_string(),
-            bucket,
-            object,
-            user,
-            time: Utc::now().to_rfc3339(),
-            user_agent: "rustfs.rs-client".to_string(),
-            span_id,
-        };
-        audit_logger.log(audit_entry).await;
-
-        log_info("query operation completed");
-    }
-    #[instrument(fields(user))]
-    fn query_one(user: String) {
-        // 初始化 OpenTelemetry Tracer
-        let tracer = global::tracer("query_one");
-        tracer.in_span("doing_work", |cx| {
-            // Traced app logic here...
-            Span::current().set_parent(cx);
-            log_info("Doing work...");
-            let current_span = Span::current();
-            let span_context = current_span.context();
-            let trace_id = span_context.clone().span().span_context().trace_id().to_string();
-            let span_id = span_context.clone().span().span_context().span_id().to_string();
-            log_info(format!("trace_id: {}, span_id: {}", trace_id, span_id).as_str());
-        });
-        log_info(format!("Starting query_one operation user:{}", user).as_str());
-    }
-    #[instrument(fields(user))]
-    fn query_two(user: String) {
-        log_info(format!("Starting query_two operation user:{}", user).as_str());
-    }
-    #[instrument(fields(user))]
-    fn query_three(user: String) {
-        log_info(format!("Starting query_three operation user: {}", user).as_str());
-    }
+/// Log module initialization function
+///
+/// Return to Logger and Clean Guard
+pub fn init_logging(config: AppConfig) -> (Logger, telemetry::OtelGuard) {
+    let guard = init_telemetry(&config.observability);
+    let sinks = sink::create_sinks(&config);
+    let logger = start_logger(&config, sinks);
+    (logger, guard)
 }
--- packages/obs/src/telemetry.rs
+++ packages/obs/src/telemetry.rs
@@ -1,149 +1,222 @@
+use crate::{get_local_ip_with_default, OtelConfig};
 use opentelemetry::trace::TracerProvider;
 use opentelemetry::{global, KeyValue};
 use opentelemetry_appender_tracing::layer;
-use opentelemetry_otlp::{self, WithExportConfig};
+use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::logs::SdkLoggerProvider;
 use opentelemetry_sdk::{
     metrics::{MeterProviderBuilder, PeriodicReader, SdkMeterProvider},
     trace::{RandomIdGenerator, Sampler, SdkTracerProvider},
     Resource,
 };
-use opentelemetry_semantic_conventions::attribute::NETWORK_LOCAL_ADDRESS;
 use opentelemetry_semantic_conventions::{
-    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, SERVICE_NAME, SERVICE_VERSION},
+    attribute::{DEPLOYMENT_ENVIRONMENT_NAME, NETWORK_LOCAL_ADDRESS, SERVICE_NAME, SERVICE_VERSION},
     SCHEMA_URL,
 };
-use std::time::Duration;
-use tracing::{info, Level};
 use tracing_opentelemetry::{MetricsLayer, OpenTelemetryLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 
-/// Telemetry is a wrapper around the OpenTelemetry SDK Tracer and Meter providers.
-/// It initializes the global Tracer and Meter providers, and sets up the tracing subscriber.
-/// The Tracer and Meter providers are shut down when the Telemetry instance is dropped.
-/// This is a convenience struct to ensure that the global providers are properly initialized and shut down.
+/// A guard object that manages the lifecycle of OpenTelemetry components.
+///
+/// This struct holds references to the created OpenTelemetry providers and ensures
+/// they are properly shut down when the guard is dropped. It implements the RAII
+/// (Resource Acquisition Is Initialization) pattern for managing telemetry resources.
+///
+/// When this guard goes out of scope, it will automatically shut down:
+/// - The tracer provider (for distributed tracing)
+/// - The meter provider (for metrics collection)
+/// - The logger provider (for structured logging)
 ///
 /// # Example
-/// ```
-/// use rustfs_obs::Telemetry;
 ///
-/// let _telemetry = Telemetry::init();
 /// ```
-pub struct Telemetry {
+/// use rustfs_obs::{init_telemetry, OtelConfig};
+///
+/// let config = OtelConfig::default();
+/// let otel_guard = init_telemetry(&config);
+///
+/// // The guard is kept alive for the duration of the application
+/// // When it's dropped, all telemetry components are properly shut down
+/// drop(otel_guard);
+/// ```
+pub struct OtelGuard {
     tracer_provider: SdkTracerProvider,
     meter_provider: SdkMeterProvider,
+    logger_provider: SdkLoggerProvider,
 }
 
-impl Telemetry {
-    pub fn init() -> Self {
-        // Define service resource information
-        let resource = Resource::builder()
-            .with_service_name("rustfs-service")
-            .with_schema_url(
-                [
-                    KeyValue::new(SERVICE_NAME, "rustfs-service"),
-                    KeyValue::new(SERVICE_VERSION, "0.1.0"),
-                    KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, "develop"),
-                    KeyValue::new(NETWORK_LOCAL_ADDRESS, "127.0.0.1"),
-                ],
-                SCHEMA_URL,
-            )
-            .build();
+impl Drop for OtelGuard {
+    fn drop(&mut self) {
+        if let Err(err) = self.tracer_provider.shutdown() {
+            eprintln!("Tracer shutdown error: {:?}", err);
+        }
+        if let Err(err) = self.meter_provider.shutdown() {
+            eprintln!("Meter shutdown error: {:?}", err);
+        }
+        if let Err(err) = self.logger_provider.shutdown() {
+            eprintln!("Logger shutdown error: {:?}", err);
+        }
+    }
+}
 
-        let tracer_exporter = opentelemetry_otlp::SpanExporter::builder()
+/// create OpenTelemetry Resource
+fn resource(config: &OtelConfig) -> Resource {
+    Resource::builder()
+        .with_service_name(config.service_name.clone())
+        .with_schema_url(
+            [
+                KeyValue::new(SERVICE_NAME, config.service_name.clone()),
+                KeyValue::new(SERVICE_VERSION, config.service_version.clone()),
+                KeyValue::new(DEPLOYMENT_ENVIRONMENT_NAME, config.deployment_environment.clone()),
+                KeyValue::new(NETWORK_LOCAL_ADDRESS, get_local_ip_with_default()),
+            ],
+            SCHEMA_URL,
+        )
+        .build()
+}
+
+/// Initialize Meter Provider
+fn init_meter_provider(config: &OtelConfig) -> SdkMeterProvider {
+    let mut builder = MeterProviderBuilder::default().with_resource(resource(config));
+    // If endpoint is empty, use stdout output
+    if config.endpoint.is_empty() {
+        builder = builder.with_reader(
+            PeriodicReader::builder(opentelemetry_stdout::MetricExporter::default())
+                .with_interval(std::time::Duration::from_secs(config.meter_interval))
+                .build(),
+        );
+    } else {
+        // If endpoint is not empty, use otlp output
+        let exporter = opentelemetry_otlp::MetricExporter::builder()
             .with_tonic()
-            .with_endpoint("http://localhost:4317")
-            .with_protocol(opentelemetry_otlp::Protocol::Grpc)
-            .with_timeout(Duration::from_secs(3))
-            .build()
-            .unwrap();
-
-        // Configure Tracer Provider
-        let tracer_provider = SdkTracerProvider::builder()
-            .with_sampler(Sampler::AlwaysOn)
-            .with_id_generator(RandomIdGenerator::default())
-            .with_resource(resource.clone())
-            .with_batch_exporter(tracer_exporter)
-            // .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
-            .build();
-
-        let meter_exporter = opentelemetry_otlp::MetricExporter::builder()
-            .with_tonic()
-            .with_endpoint("http://localhost:4317")
-            .with_protocol(opentelemetry_otlp::Protocol::Grpc)
-            .with_timeout(Duration::from_secs(3))
+            .with_endpoint(&config.endpoint)
             .with_temporality(opentelemetry_sdk::metrics::Temporality::default())
             .build()
             .unwrap();
+        builder = builder.with_reader(
+            PeriodicReader::builder(exporter)
+                .with_interval(std::time::Duration::from_secs(config.meter_interval))
+                .build(),
+        );
+        // If use_stdout is true, output to stdout at the same time
+        if config.use_stdout {
+            builder = builder.with_reader(
+                PeriodicReader::builder(opentelemetry_stdout::MetricExporter::default())
+                    .with_interval(std::time::Duration::from_secs(config.meter_interval))
+                    .build(),
+            );
+        }
+    }
 
-        let meter_reader = PeriodicReader::builder(meter_exporter)
-            .with_interval(Duration::from_secs(30))
-            .build();
+    let meter_provider = builder.build();
+    global::set_meter_provider(meter_provider.clone());
+    meter_provider
+}
 
-        // For debugging in development
-        // let meter_stdout_reader = PeriodicReader::builder(opentelemetry_stdout::MetricExporter::default()).build();
+/// Initialize Tracer Provider
+fn init_tracer_provider(config: &OtelConfig) -> SdkTracerProvider {
+    let sampler = if config.sample_ratio > 0.0 && config.sample_ratio < 1.0 {
+        Sampler::TraceIdRatioBased(config.sample_ratio)
+    } else {
+        Sampler::AlwaysOn
+    };
+    let builder = SdkTracerProvider::builder()
+        .with_sampler(sampler)
+        .with_id_generator(RandomIdGenerator::default())
+        .with_resource(resource(config));
 
-        // Configure Meter Provider
-        let meter_provider = MeterProviderBuilder::default()
-            .with_resource(resource.clone())
-            .with_reader(meter_reader)
-            // .with_reader(meter_stdout_reader)
-            .build();
+    let tracer_provider = if config.endpoint.is_empty() {
+        builder
+            .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
+            .build()
+    } else {
+        let exporter = opentelemetry_otlp::SpanExporter::builder()
+            .with_tonic()
+            .with_endpoint(&config.endpoint)
+            .build()
+            .unwrap();
+        if config.use_stdout {
+            builder
+                .with_batch_exporter(exporter)
+                .with_batch_exporter(opentelemetry_stdout::SpanExporter::default())
+        } else {
+            builder.with_batch_exporter(exporter)
+        }
+        .build()
+    };
 
-        // Set global Tracer and Meter providers
-        global::set_tracer_provider(tracer_provider.clone());
-        global::set_meter_provider(meter_provider.clone());
+    global::set_tracer_provider(tracer_provider.clone());
+    tracer_provider
+}
 
-        let tracer = tracer_provider.tracer("rustfs-service");
+/// Initialize Telemetry
+pub fn init_telemetry(config: &OtelConfig) -> OtelGuard {
+    let tracer_provider = init_tracer_provider(config);
+    let meter_provider = init_meter_provider(config);
+    let tracer = tracer_provider.tracer(config.service_name.clone());
 
-        // // let _stdout_exporter = opentelemetry_stdout::LogExporter::default();
-        // let otlp_exporter = opentelemetry_otlp::LogExporter::builder()
-        //     .with_tonic()
-        //     .with_endpoint("http://localhost:4317")
-        //     // .with_timeout(Duration::from_secs(3))
-        //     // .with_protocol(opentelemetry_otlp::Protocol::Grpc)
-        //     .build()
-        //     .unwrap();
-        // let provider: SdkLoggerProvider = SdkLoggerProvider::builder()
-        //     .with_resource(resource.clone())
-        //     .with_simple_exporter(otlp_exporter)
-        //     .build();
-        // let filter_otel = EnvFilter::new("debug")
-        //     // .add_directive("hyper=off".parse().unwrap())
-        //     // .add_directive("opentelemetry=off".parse().unwrap())
-        //     // .add_directive("tonic=off".parse().unwrap())
-        //     // .add_directive("h2=off".parse().unwrap())
-        //     .add_directive("reqwest=off".parse().unwrap());
-        // let otel_layer = layer::OpenTelemetryTracingBridge::new(&provider).with_filter(filter_otel);
+    let logger_provider = if config.endpoint.is_empty() {
+        SdkLoggerProvider::builder()
+            .with_resource(resource(config))
+            .with_simple_exporter(opentelemetry_stdout::LogExporter::default())
+            .build()
+    } else {
+        let exporter = opentelemetry_otlp::LogExporter::builder()
+            .with_tonic()
+            .with_endpoint(&config.endpoint)
+            .build()
+            .unwrap();
+        SdkLoggerProvider::builder()
+            .with_resource(resource(config))
+            .with_batch_exporter(exporter)
+            .with_batch_exporter(opentelemetry_stdout::LogExporter::default())
+            .build()
+    };
+
+    let otel_layer = layer::OpenTelemetryTracingBridge::new(&logger_provider);
+    // For the OpenTelemetry layer, add a tracing filter to filter events from
+    // OpenTelemetry and its dependent crates (opentelemetry-otlp uses crates
+    // like reqwest/tonic etc.) from being sent back to OTel itself, thus
+    // preventing infinite telemetry generation. The filter levels are set as
+    // follows:
+    // - Allow `info` level and above by default.
+    // - Restrict `opentelemetry`, `hyper`, `tonic`, and `reqwest` completely.
+    // Note: This will also drop events from crates like `tonic` etc. even when
+    // they are used outside the OTLP Exporter. For more details, see:
+    // https://github.com/open-telemetry/opentelemetry-rust/issues/761
+    let filter_otel = EnvFilter::new("info")
+        .add_directive("hyper=off".parse().unwrap())
+        .add_directive("opentelemetry=off".parse().unwrap())
+        .add_directive("tonic=off".parse().unwrap())
+        .add_directive("h2=off".parse().unwrap())
+        .add_directive("reqwest=off".parse().unwrap());
+    let otel_layer = otel_layer.with_filter(filter_otel);
+    let registry = tracing_subscriber::registry()
+        .with(tracing_subscriber::filter::LevelFilter::INFO)
+        .with(OpenTelemetryLayer::new(tracer))
+        .with(MetricsLayer::new(meter_provider.clone()))
+        .with(otel_layer);
+    if config.endpoint.is_empty() {
+        // Create a new tracing::Fmt layer to print the logs to stdout. It has a
+        // default filter of `info` level and above, and `debug` and above for logs
+        // from OpenTelemetry crates. The filter levels can be customized as needed.
         let filter_fmt = EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap());
         let fmt_layer = tracing_subscriber::fmt::layer()
             .with_thread_names(true)
             .with_filter(filter_fmt);
 
-        // Configure `tracing subscriber`
-        tracing_subscriber::registry()
-            .with(tracing_subscriber::filter::LevelFilter::from_level(Level::DEBUG))
+        registry
             .with(tracing_subscriber::fmt::layer().with_ansi(true))
-            .with(MetricsLayer::new(meter_provider.clone()))
-            .with(OpenTelemetryLayer::new(tracer))
-            // .with(otel_layer)
             .with(fmt_layer)
             .init();
-        info!(name: "my-event-name", target: "my-system", event_id = 20, user_name = "otel", user_email = "otel@opentelemetry.io", message = "This is an example message");
-        Self {
-            tracer_provider,
-            meter_provider,
-        }
+    } else {
+        registry.with(tracing_subscriber::fmt::layer().with_ansi(false)).init();
+        println!("Logs and meter,tracer enabled");
     }
-}
 
-impl Drop for Telemetry {
-    fn drop(&mut self) {
-        if let Err(err) = self.tracer_provider.shutdown() {
-            eprintln!("{err:?}");
-        }
-        if let Err(err) = self.meter_provider.shutdown() {
-            eprintln!("{err:?}");
-        }
+    OtelGuard {
+        tracer_provider,
+        meter_provider,
+        logger_provider,
     }
 }
