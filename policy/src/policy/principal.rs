93dcfc0cdd20223244d85923b24a145d660fed03
weisd <weishidavip@163.com>
2025-03-26 11:25:12 +0800

    add bucketpolicy

---
+++ policy/src/policy/principal.rs
@@ -0,0 +1,32 @@
+use super::{utils::wildcard, Validator};
+use common::error::{Error, Result};
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, Deserialize, Serialize, Default, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase", default)]
+pub struct Principal {
+    #[serde(rename = "AWS")]
+    aws: HashSet<String>,
+}
+
+impl Principal {
+    pub fn is_match(&self, parincipal: &str) -> bool {
+        for pattern in self.aws.iter() {
+            if wildcard::is_simple_match(pattern, parincipal) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+impl Validator for Principal {
+    type Error = Error;
+    fn is_valid(&self) -> Result<()> {
+        if self.aws.is_empty() {
+            return Err(Error::msg("Principal is empty"));
+        }
+        Ok(())
+    }
+}
