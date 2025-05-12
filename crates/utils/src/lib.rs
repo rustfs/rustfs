65be8145ff93b9f8bc235158c1a68c188a26135b
houseme <housemecn@gmail.com>
2025-05-12 13:32:18 +0800

    feat(obs): implement global OpenTelemetry guard management

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -2,10 +2,7 @@ mod certs;
 mod ip;
 mod net;
 
-pub use certs::certs_error;
-pub use certs::create_multi_cert_resolver;
-pub use certs::load_all_certs_from_directory;
-pub use certs::load_certs;
-pub use certs::load_private_key;
-pub use ip::get_local_ip;
-pub use ip::get_local_ip_with_default;
+#[cfg(feature = "ip")]
+pub use certs::*;
+#[cfg(feature = "ip")]
+pub use ip::*;
