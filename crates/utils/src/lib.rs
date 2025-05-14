136118ed2182b83c95b62253e3b6fe05af68202b
houseme <housemecn@gmail.com>
2025-05-14 19:04:52 +0800

    fix

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,8 +1,11 @@
+#[cfg(feature = "tls")]
 mod certs;
+#[cfg(feature = "ip")]
 mod ip;
+#[cfg(feature = "net")]
 mod net;
 
-#[cfg(feature = "ip")]
+#[cfg(feature = "tls")]
 pub use certs::*;
 #[cfg(feature = "ip")]
 pub use ip::*;
