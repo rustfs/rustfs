fffc79e981adbed962abf7f2b39829b25dcef9b8
安正超 <overtrue@anzhengchaodeMacBook-Air.local>
2025-05-28 14:38:43 +0800

    fix: revert ns_lock test to use distributed locks and add ignore attribute

--- e2e_test/src/reliant/lock.rs
+++ e2e_test/src/reliant/lock.rs
@@ -47,10 +47,11 @@ async fn test_lock_unlock_rpc() -> Result<(), Box<dyn Error>> {
 }
 
 #[tokio::test]
+#[ignore = "requires running RustFS server at localhost:9000"]
 async fn test_lock_unlock_ns_lock() -> Result<(), Box<dyn Error>> {
     let url = url::Url::parse("http://127.0.0.1:9000/data")?;
     let locker = new_lock_api(false, Some(url));
-    let ns_mutex = Arc::new(RwLock::new(NsLockMap::new(false)));
+    let ns_mutex = Arc::new(RwLock::new(NsLockMap::new(true)));
     let ns = new_nslock(
         Arc::clone(&ns_mutex),
         "local".to_string(),
