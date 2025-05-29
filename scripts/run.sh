3ceec7b5f2fc117a161967b85e525e96d5d7a757
houseme <housemecn@gmail.com>
2025-05-29 21:10:04 +0800

    improve code for obs

--- scripts/run.sh
+++ scripts/run.sh
@@ -31,16 +31,16 @@ export RUSTFS_VOLUMES="./target/volume/test{0...4}"
 # export RUSTFS_VOLUMES="./target/volume/test"
 export RUSTFS_ADDRESS=":9000"
 export RUSTFS_CONSOLE_ENABLE=true
-export RUSTFS_CONSOLE_ADDRESS=":9002"
+export RUSTFS_CONSOLE_ADDRESS=":9001"
 # export RUSTFS_SERVER_DOMAINS="localhost:9000"
 # HTTPS 证书目录
 # export RUSTFS_TLS_PATH="./deploy/certs"
 
 # 具体路径修改为配置文件真实路径，obs.example.toml 仅供参考 其中 `RUSTFS_OBS_CONFIG` 和下面变量二选一
-# export RUSTFS_OBS_CONFIG="./deploy/config/obs.toml"
+# export RUSTFS_OBS_ENDPOINT=http://localhost:4317
 
 # 如下变量需要必须参数都有值才可以，以及会覆盖配置文件中的值
-export RUSTFS_OBSERVABILITY_ENDPOINT=http://localhost:4317
+#export RUSTFS_OBSERVABILITY_ENDPOINT=http://localhost:4317
 #export RUSTFS_OBSERVABILITY_ENDPOINT=http://localhost:4317
 #export RUSTFS_OBSERVABILITY_USE_STDOUT=false
 #export RUSTFS_OBSERVABILITY_SAMPLE_RATIO=2.0
@@ -49,27 +49,29 @@ export RUSTFS_OBSERVABILITY_ENDPOINT=http://localhost:4317
 #export RUSTFS_OBSERVABILITY_SERVICE_VERSION=0.1.0
 #export RUSTFS_OBSERVABILITY_ENVIRONMENT=develop
 #export RUSTFS_OBSERVABILITY_LOGGER_LEVEL=debug
-#export RUSTFS_OBSERVABILITY_LOCAL_LOGGING_ENABLED=true
+export RUSTFS_OBSERVABILITY_LOCAL_LOGGING_ENABLED=true
+#export RUSTFS_OBSERVABILITY_LOCAL_LOGGING_ENABLED=false # Whether to enable local logging
+export RUSTFS_OBSERVABILITY_LOG_DIRECTORY="./deploy/logs" # Log directory
+export RUSTFS_OBSERVABILITY_LOG_ROTATION_TIME="minute" # Log rotation time unit, can be "second", "minute", "hour", "day"
+export RUSTFS_OBSERVABILITY_LOG_ROTATION_SIZE_MB=1 # Log rotation size in MB
+
 #
-#export RUSTFS__SINKS_0__type=File
-#export RUSTFS__SINKS_0__path=./deploy/logs/rustfs.log
-#export RUSTFS__SINKS_0__buffer_size=12
-#export RUSTFS__SINKS_0__flush_interval_ms=1000
-#export RUSTFS__SINKS_0__flush_threshold=100
+#export RUSTFS_SINKS_FILE_PATH=./deploy/logs/rustfs.log
+#export RUSTFS_SINKS_FILE_BUFFER_SIZE=12
+#export RUSTFS_SINKS_FILE_FLUSH_INTERVAL_MS=1000
+#export RUSTFS_SINKS_FILE_FLUSH_THRESHOLD=100
 #
-#export RUSTFS__SINKS_1__type=Kakfa
-#export RUSTFS__SINKS_1__brokers=localhost:9092
-#export RUSTFS__SINKS_1__topic=logs
-#export RUSTFS__SINKS_1__batch_size=100
-#export RUSTFS__SINKS_1__batch_timeout_ms=1000
+#export RUSTFS_SINKS_KAKFA_BROKERS=localhost:9092
+#export RUSTFS_SINKS_KAKFA_TOPIC=logs
+#export RUSTFS_SINKS_KAKFA_BATCH_SIZE=100
+#export RUSTFS_SINKS_KAKFA_BATCH_TIMEOUT_MS=1000
 #
-#export RUSTFS__SINKS_2__type=Webhook
-#export RUSTFS__SINKS_2__endpoint=http://localhost:8080/webhook
-#export RUSTFS__SINKS_2__auth_token=you-auth-token
-#export RUSTFS__SINKS_2__batch_size=100
-#export RUSTFS__SINKS_2__batch_timeout_ms=1000
+#export RUSTFS_SINKS_WEBHOOK_ENDPOINT=http://localhost:8080/webhook
+#export RUSTFS_SINKS_WEBHOOK_AUTH_TOKEN=you-auth-token
+#export RUSTFS_SINKS_WEBHOOK_BATCH_SIZE=100
+#export RUSTFS_SINKS_WEBHOOK_BATCH_TIMEOUT_MS=1000
 #
-#export RUSTFS__LOGGER__QUEUE_CAPACITY=10
+#export RUSTFS_LOGGER_QUEUE_CAPACITY=10
 
 export OTEL_INSTRUMENTATION_NAME="rustfs"
 export OTEL_INSTRUMENTATION_VERSION="0.1.1"
