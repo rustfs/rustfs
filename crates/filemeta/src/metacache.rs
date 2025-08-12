a5a3fae7e9c59dce9a4d3e3603f8efe9d6457d44
junxiang Mu <1948535941@qq.com>
2025-08-12 16:54:09 +0800

    Fix: fmt && clippy
    Signed-off-by: junxiang Mu <1948535941@qq.com>

--- crates/filemeta/src/metacache.rs
+++ crates/filemeta/src/metacache.rs
@@ -826,7 +826,11 @@ impl<T: Clone + Debug + Send + 'static> Cache<T> {
                 } else {
                     Some(unsafe { (*v_ptr).clone() })
                 };
-                if let Some(updated) = v { Ok(updated) } else { Err(std::io::Error::other("cache empty after update")) }
+                if let Some(updated) = v {
+                    Ok(updated)
+                } else {
+                    Err(std::io::Error::other("cache empty after update"))
+                }
             }
             Err(err) => Err(err),
         }
