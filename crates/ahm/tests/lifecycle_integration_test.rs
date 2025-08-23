fd2aab2bd98e9b1170d54a40a4ec91da6c8814c1
houseme <housemecn@gmail.com>
2025-08-23 17:30:06 +0800

    fix：revet #443 #446 (#452)
    * fix： revet #443 #446
    
    * fix

--- crates/ahm/tests/lifecycle_integration_test.rs
+++ crates/ahm/tests/lifecycle_integration_test.rs
@@ -1,3 +1,17 @@
+// Copyright 2024 RustFS Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use rustfs_ahm::scanner::{Scanner, data_scanner::ScannerConfig};
 use rustfs_ecstore::{
     bucket::metadata::BUCKET_LIFECYCLE_CONFIG,
@@ -151,7 +165,6 @@ async fn object_exists(ecstore: &Arc<ECStore>, bucket: &str, object: &str) -> bo
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
 #[serial]
-#[ignore = "Please run it manually."]
 async fn test_lifecycle_expiry_basic() {
     let (_disk_paths, ecstore) = setup_test_env().await;
 
