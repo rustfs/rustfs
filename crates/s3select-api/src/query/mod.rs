11bcb46ea9e260c3a58e1930f624a258c50fdda9
houseme <housemecn@gmail.com>
2025-12-24 18:45:31 +0800

    set object_store 0.12.4

--- crates/s3select-api/src/query/mod.rs
+++ crates/s3select-api/src/query/mod.rs
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::Arc;
-
 use s3s::dto::SelectObjectContentInput;
+use std::sync::Arc;
 
 pub mod analyzer;
 pub mod ast;
