3d1eb3004bde8127a9744d50974aa7f995a8b754
GatewayJ <835269233@qq.com>
2026-01-09 23:21:14 +0800

    feat: object-list access

--- rustfs/src/storage/access.rs
+++ rustfs/src/storage/access.rs
@@ -818,10 +818,10 @@ impl S3Access for FS {
     }
 
     /// Checks whether the ListObjectVersions request has accesses to the resources.
-    ///
-    /// This method returns `Ok(())` by default.
-    async fn list_object_versions(&self, _req: &mut S3Request<ListObjectVersionsInput>) -> S3Result<()> {
-        Ok(())
+    async fn list_object_versions(&self, req: &mut S3Request<ListObjectVersionsInput>) -> S3Result<()> {
+        let req_info = req.extensions.get_mut::<ReqInfo>().expect("ReqInfo not found");
+        req_info.bucket = Some(req.input.bucket.clone());
+        authorize_request(req, Action::S3Action(S3Action::ListBucketVersionsAction)).await
     }
 
     /// Checks whether the ListObjects request has accesses to the resources.
