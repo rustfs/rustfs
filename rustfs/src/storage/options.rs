a3f01ab1ae03eb2be25413155549a150d4a1c896
houseme <housemecn@gmail.com>
2026-01-28 03:30:36 +0800

    fix

--- rustfs/src/storage/options.rs
+++ rustfs/src/storage/options.rs
@@ -1315,7 +1315,7 @@ mod tests {
         // %FF is invalid UTF-8 when percent-decoded into bytes.
         let uri: Uri = "/b/k?x-amz-meta-a=%FF".parse().unwrap();
         let err = extract_query_meta_from_uri(&uri).unwrap_err();
-        assert!(err.message().unwrap().to_ascii_lowercase().contains("utf-8"));
+        assert!(err.to_string().to_ascii_lowercase().contains("utf-8"));
     }
 
     #[test]
@@ -1323,7 +1323,7 @@ mod tests {
         // newline is not allowed
         let uri: Uri = "/b/k?x-amz-meta-a=hello%0Aworld".parse().unwrap();
         let err = extract_query_meta_from_uri(&uri).unwrap_err();
-        assert!(err.message().unwrap().to_ascii_lowercase().contains("printable"));
+        assert!(err.to_string().to_ascii_lowercase().contains("printable"));
     }
 
     #[test]
@@ -1331,7 +1331,7 @@ mod tests {
         let big = "a".repeat(QUERY_META_MAX_ENTRY_BYTES + 1);
         let uri: Uri = format!("/b/k?x-amz-meta-a={big}").parse().unwrap();
         let err = extract_query_meta_from_uri(&uri).unwrap_err();
-        assert!(err.message().unwrap().to_ascii_lowercase().contains("entry too large"));
+        assert!(err.to_string().to_ascii_lowercase().contains("entry too large"));
     }
 
     #[test]
@@ -1346,6 +1346,6 @@ mod tests {
         }
         let uri: Uri = format!("/b/k?{qs}").parse().unwrap();
         let err = extract_query_meta_from_uri(&uri).unwrap_err();
-        assert!(err.message().unwrap().to_ascii_lowercase().contains("total too large"));
+        assert!(err.to_string().to_ascii_lowercase().contains("total too large"));
     }
 }
