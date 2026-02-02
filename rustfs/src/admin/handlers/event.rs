4a950be182d877775d7831ce3890ec90d8ae33c7
houseme <housemecn@gmail.com>
2026-02-02 19:03:54 +0800

    fix: Fixed that `account_id` returns normal value

--- rustfs/src/admin/handlers/event.rs
+++ rustfs/src/admin/handlers/event.rs
@@ -260,7 +260,7 @@ impl Operation for ListNotificationTargets {
                     _ => "offline",
                 };
                 NotificationEndpoint {
-                    account_id: target.id().to_string(),
+                    account_id: target.id().id.clone(),
                     service: target.name().to_string(),
                     status: status.to_string(),
                 }
