fc8c928be2f425cf985cf8136618e95d89ad50b1
overtrue <anzhengchao@gmail.com>
2026-02-19 23:06:44 +0800

    refactor(storage): extract tagging helpers from ecfs

--- rustfs/src/storage/ecfs.rs
+++ rustfs/src/storage/ecfs.rs
@@ -34,6 +34,10 @@ use crate::storage::s3_api::multipart::{
 use crate::storage::s3_api::response::{
     access_denied_error, map_abort_multipart_upload_error, not_initialized_error, s3_response,
 };
+use crate::storage::s3_api::tagging::{
+    build_delete_object_tagging_output, build_get_bucket_tagging_output, build_get_object_tagging_output,
+    build_put_object_tagging_output, validate_object_tag_set,
+};
 use crate::storage::sse::{
     DecryptionRequest, EncryptionRequest, PrepareEncryptionRequest, check_encryption_metadata, sse_decryption, sse_encryption,
     sse_prepare_encryption, strip_managed_encryption_metadata,
@@ -1601,7 +1605,7 @@ impl S3 for FS {
         let version_id_resp = version_id.clone().unwrap_or_default();
         helper = helper.version_id(version_id_resp);
 
-        let result = Ok(s3_response(DeleteObjectTaggingOutput { version_id }));
+        let result = Ok(s3_response(build_delete_object_tagging_output(version_id)));
         let _ = helper.complete(&result);
         let duration = start_time.elapsed();
         histogram!("rustfs.object_tagging.operation.duration.seconds", "operation" => "delete").record(duration.as_secs_f64());
@@ -2265,7 +2269,7 @@ impl S3 for FS {
             }
         };
 
-        Ok(s3_response(GetBucketTaggingOutput { tag_set }))
+        Ok(s3_response(build_get_bucket_tagging_output(tag_set)))
     }
 
     #[instrument(level = "debug", skip(self))]
@@ -3086,10 +3090,7 @@ impl S3 for FS {
         counter!("rustfs.get_object_tagging.success").increment(1);
         let duration = start_time.elapsed();
         histogram!("rustfs.object_tagging.operation.duration.seconds", "operation" => "put").record(duration.as_secs_f64());
-        Ok(s3_response(GetObjectTaggingOutput {
-            tag_set,
-            version_id: req.input.version_id.clone(),
-        }))
+        Ok(s3_response(build_get_object_tagging_output(tag_set, req.input.version_id.clone())))
     }
 
     #[instrument(level = "debug", skip(self, _req))]
@@ -4251,48 +4252,11 @@ impl S3 for FS {
             ..
         } = req.input.clone();
 
-        if tagging.tag_set.len() > 10 {
-            // TOTO: Note that Amazon S3 limits the maximum number of tags to 10 tags per object.
-            // Reference: https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-tagging.html
-            // Reference: https://docs.aws.amazon.com/zh_cn/AmazonS3/latest/API/API_PutObjectTagging.html
-            // https://github.com/minio/mint/blob/master/run/core/aws-sdk-go-v2/main.go#L1647
-            error!("Tag set exceeds maximum of 10 tags: {}", tagging.tag_set.len());
-            return Err(s3_error!(InvalidTag, "Cannot have more than 10 tags per object"));
-        }
+        validate_object_tag_set(&tagging.tag_set)?;
 
         let Some(store) = new_object_layer_fn() else {
             return Err(not_initialized_error());
         };
-
-        let mut tag_keys = std::collections::HashSet::with_capacity(tagging.tag_set.len());
-        for tag in &tagging.tag_set {
-            let key = tag.key.as_ref().filter(|k| !k.is_empty()).ok_or_else(|| {
-                error!("Empty tag key");
-                s3_error!(InvalidTag, "Tag key cannot be empty")
-            })?;
-
-            if key.len() > 128 {
-                error!("Tag key too long: {} bytes", key.len());
-                return Err(s3_error!(InvalidTag, "Tag key is too long, maximum allowed length is 128 characters"));
-            }
-
-            // allow to set the value of a tag to an empty string, but cannot set it to a null value.
-            // Reference：https://docs.aws.amazon.com/zh_cn/AWSEC2/latest/UserGuide/Using_Tags.html#tag-restrictions
-            let value = tag.value.as_ref().ok_or_else(|| {
-                error!("Null tag value");
-                s3_error!(InvalidTag, "Tag value cannot be null")
-            })?;
-
-            if value.len() > 256 {
-                error!("Tag value too long: {} bytes", value.len());
-                return Err(s3_error!(InvalidTag, "Tag value is too long, maximum allowed length is 256 characters"));
-            }
-
-            if !tag_keys.insert(key) {
-                error!("Duplicate tag key: {}", key);
-                return Err(s3_error!(InvalidTag, "Cannot provide multiple Tags with the same key"));
-            }
-        }
 
         let tags = encode_tags(tagging.tag_set);
         debug!("Encoded tags: {}", tags);
@@ -4328,9 +4292,7 @@ impl S3 for FS {
         let version_id_resp = req.input.version_id.clone().unwrap_or_default();
         helper = helper.version_id(version_id_resp);
 
-        let result = Ok(s3_response(PutObjectTaggingOutput {
-            version_id: req.input.version_id.clone(),
-        }));
+        let result = Ok(s3_response(build_put_object_tagging_output(req.input.version_id.clone())));
         let _ = helper.complete(&result);
         let duration = start_time.elapsed();
         histogram!("rustfs.object_tagging.operation.duration.seconds", "operation" => "put").record(duration.as_secs_f64());
