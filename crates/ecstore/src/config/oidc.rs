55d0275ecba63e000dc8e9beac3177df0ca7fa99
Andy Teijelo <ateijelo@gmail.com>
2026-04-19 22:29:59 -0400

    more places where other_audiences config must be processed

--- crates/ecstore/src/config/oidc.rs
+++ crates/ecstore/src/config/oidc.rs
@@ -19,7 +19,7 @@ use rustfs_config::{
         OIDC_CLAIM_NAME, OIDC_CLAIM_PREFIX, OIDC_CLIENT_ID, OIDC_CLIENT_SECRET, OIDC_CONFIG_URL, OIDC_DEFAULT_CLAIM_NAME,
         OIDC_DEFAULT_EMAIL_CLAIM, OIDC_DEFAULT_GROUPS_CLAIM, OIDC_DEFAULT_ROLES_CLAIM, OIDC_DEFAULT_SCOPES,
         OIDC_DEFAULT_USERNAME_CLAIM, OIDC_DISPLAY_NAME, OIDC_EMAIL_CLAIM, OIDC_GROUPS_CLAIM, OIDC_REDIRECT_URI,
-        OIDC_REDIRECT_URI_DYNAMIC, OIDC_ROLE_POLICY, OIDC_ROLES_CLAIM, OIDC_SCOPES, OIDC_USERNAME_CLAIM,
+        OIDC_REDIRECT_URI_DYNAMIC, OIDC_ROLE_POLICY, OIDC_ROLES_CLAIM, OIDC_SCOPES, OIDC_OTHER_AUDIENCES, OIDC_USERNAME_CLAIM,
     },
 };
 use std::sync::LazyLock;
@@ -50,6 +50,11 @@ pub static DEFAULT_IDENTITY_OPENID_KVS: LazyLock<KVS> = LazyLock::new(|| {
         KV {
             key: OIDC_SCOPES.to_owned(),
             value: OIDC_DEFAULT_SCOPES.to_owned(),
+            hidden_if_empty: false,
+        },
+        KV {
+            key: OIDC_OTHER_AUDIENCES.to_owned(),
+            value: "".to_owned(),
             hidden_if_empty: false,
         },
         KV {
