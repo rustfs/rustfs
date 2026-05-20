11054263c13fd3f4626534fa1bfd78ecc4fa0dc0
安正超 <anzhengchao@gmail.com>
2026-05-20 12:00:32 +0800

    fix(utils): map common Linux filesystem magic values (#3023)

--- crates/utils/src/os/linux.rs
+++ crates/utils/src/os/linux.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::os::{DiskInfo, IOStats};
+use super::{DiskInfo, IOStats, fs_type::get_fs_type};
 use rustix::fs::statfs;
 use std::collections::BTreeSet;
 use std::fs;
@@ -84,35 +84,6 @@ pub fn get_info(p: impl AsRef<Path>) -> std::io::Result<DiskInfo> {
         minor: rustix::fs::minor(st.st_dev) as u64,
         ..Default::default()
     })
-}
-
-/// Returns the filesystem type of the underlying mounted filesystem
-///
-/// TODO The following mapping could not find the corresponding constant in `nix`:
-///
-/// "137d" => "EXT",
-/// "4244" => "HFS",
-/// "5346544e" => "NTFS",
-/// "61756673" => "AUFS",
-/// "ef51" => "EXT2OLD",
-/// "2fc12fc1" => "zfs",
-/// "ff534d42" => "cifs",
-/// "53464846" => "wslfs",
-fn get_fs_type(fs_type: u64) -> &'static str {
-    // Magic numbers for various filesystems
-    match fs_type {
-        0x01021994 => "TMPFS",
-        0x4d44 => "MSDOS",
-        0x6969 => "NFS",
-        0xEF53 => "EXT4",
-        0xf15f => "ecryptfs",
-        0x794c7630 => "overlayfs",
-        0x52654973 => "REISERFS",
-        // Additional common ones can be added here:
-        // 0x58465342 => "XFS",
-        // 0x9123683E => "BTRFS",
-        _ => "UNKNOWN",
-    }
 }
 
 pub fn same_disk(disk1: &str, disk2: &str) -> std::io::Result<bool> {
