aecac5c0ae13a0dac2149c915f3a3ac6977813c9
Zhengchao An <anzhengchao@gmail.com>
2026-07-03 11:36:14 +0800

    refactor(replication): isolate object decision contracts (#4219)

---
+++ crates/ecstore/src/bucket/replication/replication_object_decision_boundary.rs
@@ -0,0 +1,22 @@
+// Copyright 2024 RustFS Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub use rustfs_replication::MustReplicateOptions;
+pub(crate) use rustfs_replication::{
+    ReplicationDeleteSource, ReplicationMultipartPartInput, ReplicationResyncTargetObject,
+    delete_replication_missing_source_decision, delete_replication_object_opts, heal_uses_delete_replication_path,
+    is_retryable_delete_replication_head_error, is_version_delete_replication, replication_etags_match,
+    replication_multipart_complete_actual_size, replication_multipart_part_plan, resync_target_for_object,
+    should_retry_delete_marker_purge,
+};
