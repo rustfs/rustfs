2e5cef513fb31e25940f1c77e208f28b302561bb
Zhengchao An <anzhengchao@gmail.com>
2026-07-30 09:31:28 +0800

    chore(release): prepare 1.0.0-beta.12 (#5461)
    * chore(release): prepare 1.0.0-beta.12
    
    * chore(release): align release assets for 1.0.0-beta.12
    
    * chore(deps): refresh release dependencies
    
    Co-Authored-By: heihutu <heihutu@gmail.com>
    
    ---------
    
    Co-authored-by: houseme <housemecn@gmail.com>
    Co-authored-by: heihutu <heihutu@gmail.com>

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
         {
           default = rustPlatform.buildRustPackage {
             pname = "rustfs";
-            version = "1.0.0-beta.11";
+            version = "1.0.0-beta.12";
 
             src = ./.;
 
