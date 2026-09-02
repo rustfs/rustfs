b727e669dbd6b3370ff364ab25299138d99e6538
Zhengchao An <anzhengchao@gmail.com>
2026-09-03 03:38:40 +0800

    fix(object): route write-back test wire types through the s3 facade (#7080)

--- rustfs/src/app/object/on_demand_migration_put.rs
+++ rustfs/src/app/object/on_demand_migration_put.rs
@@ -278,16 +278,16 @@ mod tests {
     use super::*;
     use crate::app::storage_api::multipart_usecase::contract::multipart::MultipartOperations as _;
     use crate::app::storage_api::object_usecase::on_demand_migration::{PullFailureReason, SourceSse};
+    use crate::app::storage_api::s3::{
+        BucketVersioningStatus, DeleteMarkerReplication, DeleteMarkerReplicationStatus, Destination, ReplicationConfiguration,
+        ReplicationRule, ReplicationRuleStatus, ServerSideEncryptionByDefault, ServerSideEncryptionConfiguration,
+        ServerSideEncryptionRule, VersioningConfiguration,
+    };
     use crate::app::storage_api::test::bucket::utils::serialize;
     use crate::app::storage_api::test::contract::bucket::{BucketOperations as _, MakeBucketOptions};
     use crate::app::storage_api::test::{get_global_bucket_metadata_sys, set_bucket_metadata};
     use http::Method;
     use rustfs_utils::http::{MINIO_INTERNAL_PREFIX, RUSTFS_INTERNAL_PREFIX, get_str};
-    use s3s::dto::{
-        BucketVersioningStatus, DeleteMarkerReplication, DeleteMarkerReplicationStatus, Destination, ReplicationConfiguration,
-        ReplicationRule, ReplicationRuleStatus, ServerSideEncryptionByDefault, ServerSideEncryptionConfiguration,
-        ServerSideEncryptionRule, VersioningConfiguration,
-    };
     use sha2::{Digest as Sha256Digest, Sha256};
     use std::time::SystemTime;
     use tokio::io::AsyncReadExt;
